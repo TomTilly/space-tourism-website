e1bbf1f1feb7b3e6cbaf4c57bf81844c1dbabdd0
Tom Tillistrand <tomtillistrand@gmail.com>
2021-11-13 18:59:33 -0500

    Make sure Eleventy copies assets to the output folder

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,6 @@
 module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addPassthroughCopy('./src/assets/');
   eleventyConfig.addPassthroughCopy('./src/styles/');
   eleventyConfig.addWatchTarget('./src/styles/');
   return {
