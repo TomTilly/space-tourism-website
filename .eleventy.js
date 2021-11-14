e6481dceba10c195fb46aeeea19503969036d7f3
Tom Tillistrand <tomtillistrand@gmail.com>
2021-11-14 00:37:11 -0500

    Add desktop navigation menu

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,14 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = function(eleventyConfig) {
+
+  // https://www.11ty.dev/docs/copy/
   eleventyConfig.addPassthroughCopy('./src/assets/');
   eleventyConfig.addPassthroughCopy('./src/styles/');
   eleventyConfig.addWatchTarget('./src/styles/');
 
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
-  
+
   return {
     dir: {
       input: 'src',
