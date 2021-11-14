5a1f2548e031f82cb050f29a19c0fde8f2f7f2d1
Tom Tillistrand <tomtillistrand@gmail.com>
2021-11-13 22:21:59 -0500

    Add navigation plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,12 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/assets/');
   eleventyConfig.addPassthroughCopy('./src/styles/');
   eleventyConfig.addWatchTarget('./src/styles/');
+
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  
   return {
     dir: {
       input: 'src',
